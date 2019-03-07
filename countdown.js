7de93f3bfae4de7d8b86f6ae4c07a69f9f0b7728
MattTurnip <mattadavey@gmail.com>
2019-03-07 22:21:51 +0000

    implemented countdownGenerator function

--- countdown.js
+++ countdown.js
@@ -1,11 +1,51 @@
 var countdownGenerator = function (x) {
-    /* your code here */
-  };
+    var count = x;
+
+        return function (){
+        if (count < 0) {
+            count-- 
+            console.log ("Rockets already gone, bub!")
+        } else if (count == 0){
+            count --
+            console.log("Blast Off!")
+        } else {
+            console.log("T-Minus " + count--)
+        }
+    }
+}
   
-  var countdown = countdownGenerator(3);
+  var countdown = countdownGenerator(10);
+  
+  
   countdown(); // T-minus 3...
   countdown(); // T-minus 2...
   countdown(); // T-minus 1...
   countdown(); // Blast Off!
   countdown(); // Rockets already gone, bub!
   countdown(); // Rockets already gone, bub!
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+  countdown();
+
+
+
+  //--------------
+
+//   var foo = (function() {
+//     var x = 10;
+  
+//     var inner = function() {
+//       console.log("Value of x is: " + x);
+//     }
+//     return inner;
+//   })(); // Notice the () at the end,
+//         // which will immediately invoke the function,
+//         // assigning the returned value (inner) to foo
+  
+//   foo();
